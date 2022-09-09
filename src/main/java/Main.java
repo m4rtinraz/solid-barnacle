c56f64c44e522e0167e3431836e8c30feccea257
Martin Rodriguez az <111883228+m4rtinraz@users.noreply.github.com>
2022-09-09 07:43:34 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     
     @RequestMapping("/prueba")
     String home2() {
-        return "Hello World - v4!";
+        return "API Martin Rodriguez";
     }
 
     public static void main(String[] args) throws Exception {
